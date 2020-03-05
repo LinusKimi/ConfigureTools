e527e5045c63af69273d9647a337f7060b0a49ce
c.lv <airlcade@163.com>
2020-03-05 21:15:01 +0800

    暂存

---
+++ Core/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interface;
+
+namespace Core
+{
+    class Logger : ILogger
+    {
+        private static NLog.Logger _logger;
+        public void ShutDown() => NLog.LogManager.Shutdown();
+
+        public void WriteToConsole(string msg) => _logger.Info(msg);
+
+        public void WriteToLog(string msg) => _logger.Debug(msg);
+    }
+}
---
+++ Core/UartServer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interface;
+using System.IO;
+using System.IO.Ports;
+
+namespace Core
+{
+    public class UartServer : IService
+    {
+
+        public UartServer()
+        {
+            
+        }
+
+        public void CloseServer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenServer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
